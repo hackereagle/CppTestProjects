d42841c6d24687d28776dc39de938374447ccf17
sc.lin <read0@DESKTOP-HDLMPH8>
2020-10-17 16:23:16 +0800

    1. modily X Macro example to moew fitrwe situation. 2. change all project output path to same.

--- Source/TestXMacro/Source.cpp
+++ Source/TestXMacro/Source.cpp
@@ -11,12 +11,19 @@ enum eOption
 	option3
 };
 
+char* OptStr[] = {"option1", "option2", "option3" };
+
+const char* enum2str(eOption idx)
+{
+	return OptStr[idx];
+}
+
 int main(int argc, char* argv[])
 {
-	char* OptStr[] = {"option1", "option2", "option3" };
 
-	for (int i = 0; i < 3; i++)
-		printf("%s\n", OptStr[i]);
+	printf("%s\n", enum2str(eOption::option1));
+	printf("%s\n", enum2str(eOption::option2));
+	printf("%s\n", enum2str(eOption::option3));
 
 	system("pause");
 	return 1;
@@ -26,26 +33,36 @@ int main(int argc, char* argv[])
 // Here using X Macro
 #include <iostream>
 
+// define TABLE
 #define TABLE \
 	SUBSTITUTED(option1, "option1") \
 	SUBSTITUTED(option2, "option2") \
 	SUBSTITUTED(option3, "option3") 
 
+// using TABLE to generate enum
 #define SUBSTITUTED(a, b) a, 
-enum Option 
-{
-	TABLE
-};
+	enum eOption 
+	{
+		TABLE
+	};
 #undef SUBSTITUTED
 
-int main(int argc, char* argv[])
-{
+// using TABLE to generate string array
 #define SUBSTITUTED(a, b) b,
 	char* OptStr[] = {TABLE};
 #undef SUBSTITUTED
 
-	for (int i = 0; i < 3; i++)
-		printf("%s\n", OptStr[i]);
+const char* enum2str(eOption idx)
+{
+	return OptStr[idx];
+}
+
+int main(int argc, char* argv[])
+{
+
+	printf("%s\n", enum2str(eOption::option1));
+	printf("%s\n", enum2str(eOption::option2));
+	printf("%s\n", enum2str(eOption::option3));
 
 	system("pause");
 	return 1;
